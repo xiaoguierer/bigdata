1f4ff25953c021ebbbfbba83c124ca74e723357c
xiaoguierer <pkali@sohu.com>
2017-10-19 15:15:58 +0800

    first commit

---
+++ cache/src/test/java/RedisClientTest.java
@@ -0,0 +1,12 @@
+import com.bigdata.cache.laosiji.redis.RedisClient;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class RedisClientTest {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        new RedisClient().show();
+      }
+
+    }
