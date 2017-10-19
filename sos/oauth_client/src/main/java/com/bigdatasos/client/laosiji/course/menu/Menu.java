342130d4a43fea02c2e8e9f8dfc8cfb1fad68f4f
xiaoguierer <pkali@sohu.com>
2017-10-19 15:41:17 +0800

    first commit

---
+++ sos/oauth_client/src/main/java/com/bigdatasos/client/laosiji/course/menu/Menu.java
@@ -0,0 +1,19 @@
+package com.bigdatasos.client.laosiji.course.menu;
+
+/**
+ * 菜单
+ * 
+ * @author liufeng
+ * @date 2013-10-15
+ */
+public class Menu {
+	private Button[] button;
+
+	public Button[] getButton() {
+		return button;
+	}
+
+	public void setButton(Button[] button) {
+		this.button = button;
+	}
+}
